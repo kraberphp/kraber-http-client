75ac3c3724ff643a6789077bf7644888e8738875
Florian Pisani <42639605+f-pisani@users.noreply.github.com>
2021-06-23 20:57:04 +0200

    Normalized repository structure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,3 @@
 declare(strict_types=1);
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->addPsr4('Kraber\\Test\\', __DIR__ . "/");
-$loader->addPsr4('Kraber\\Test\\Unit\\', __DIR__ . "/unit");
-$loader->addPsr4('Kraber\\Test\\Integration\\', __DIR__ . "/integration");
